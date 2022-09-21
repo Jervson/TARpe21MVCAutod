83441fdd51f7065f3249b427c1dcabd2df16f088
opilane <opilane@HP-CZC6297B8T>
2022-09-21 11:15:36 +0300

    Add project files.

---
+++ Data/MVCAutodContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCAutod.Models;
+
+namespace MVCAutod.Data
+{
+    public class MVCAutodContext : DbContext
+    {
+        public MVCAutodContext (DbContextOptions<MVCAutodContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCAutod.Models.Auto> Auto { get; set; }
+    }
+}
---
+++ Models/Auto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAutod.Models
+{
+    public class Auto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Brand { get; set; }
+        public decimal Color { get; set; }
+    }
+}
