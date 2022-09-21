c9acec3e7034eaa79ce53d9865c57776d7394111
Jervson <henri.jervson@gmail.com>
2022-09-21 11:45:42 +0300

    scaffold ja database

--- Models/Auto.cs
+++ Models/Auto.cs
@@ -9,11 +9,11 @@ namespace MVCAutod.Models
     public class Auto
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Model { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Brand { get; set; }
-        public decimal Color { get; set; }
+        public decimal Price { get; set; }
     }
 }
---
+++ Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCAutod.Data;
+using MVCAutod.Models;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAutodContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCAutodContext>>()))
+            {
+
+                if (context.Auto.Any())
+                {
+                    return;  
+                }
+
+                context.Auto.AddRange(
+                    new Auto
+                    {
+                        Model = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Brand = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Auto
+                    {
+                        Model = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Brand = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Auto
+                    {
+                        Model = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Brand = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Auto
+                    {
+                        Model = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Brand = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
