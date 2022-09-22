a88b7920ad7c6de22716283ac8f20f42d72bcb4c
Jervson <Henri.jervson@gmail.com>
2022-09-22 15:50:26 +0300

    migratsioonid

---
+++ Migrations/20220922111625_Titel into Model and Color into Price.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCAutod.Migrations
+{
+    public partial class TitelintoModelandColorintoPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Auto",
+                newName: "Model");
+
+            migrationBuilder.RenameColumn(
+                name: "Color",
+                table: "Auto",
+                newName: "Price");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Auto",
+                newName: "Color");
+
+            migrationBuilder.RenameColumn(
+                name: "Model",
+                table: "Auto",
+                newName: "Title");
+        }
+    }
+}
